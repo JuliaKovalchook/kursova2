200e6de9287abf666c588559ae42ecdb8555debb
JuliaKovalchook <uliakovalchuk@gmail.comheroku>
2020-01-05 22:55:37 +0300

    -

--- ORM.py
+++ ORM.py
@@ -183,8 +183,8 @@ class products(db.Model):
 
     __tablename__ = 'products'
     name_product = db.Column('name_product', db.String(64), primary_key=True)
-    price = db.Column('price', db.String(64), nullable=False)
-    provider_name_provider = db.Column('provider_name_provider', db.String(64), db.ForeignKey('providers.name_provider'), nullable=False)
+    price = db.Column('price', db.Integer, nullable=False)
+    provider_name_provider = db.Column('provider_name_provider', db.String(64), db.ForeignKey('providers.name_provider'), primary_key=True)
 
     def __init__(self, name_product, price, provider_name_provider):
 
