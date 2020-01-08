bf41666d57fce4677525303a83ee8b89209dab7c
Yuliia Kovalchuk <uliakovalchook@gmail.com>
2020-01-08 17:49:52 +0200

    -

--- postgresql/create.sql
+++ postgresql/create.sql
@@ -1,39 +1,26 @@
-CREATE TABLE Groups(code text);
-ALTER TABLE Groups
-ADD CONSTRAINT code_pk PRIMARY KEY (code);
 
-CREATE TABLE Subjects(name text);
-ALTER TABLE Subjects
-ADD CONSTRAINT name_pk PRIMARY KEY(name);
 
-CREATE TABLE Subjects_Marks(subj_name text, curr_max_mark numeric NOT NULL,
-						  actual_date DATE);
-ALTER TABLE Subjects_Marks
-ADD CONSTRAINT time_name_pk PRIMARY KEY(actual_date, subj_name),
-ADD CONSTRAINT name_fk FOREIGN KEY(subj_name) REFERENCES Subjects(name);
+CREATE TABLE providers(name_provider text, type_product text);
+ALTER TABLE providers
+ADD CONSTRAINT name_provider_pk PRIMARY KEY(name_provider);
 
-CREATE TABLE Students(first_name text NOT NULL, last_name text NOT NULL,
-					  study_book text, group_code text);					 
-ALTER TABLE Students
-ADD CONSTRAINT group_spooky_bk PRIMARY KEY(group_code, study_book),
-ADD CONSTRAINT group_code_fk FOREIGN KEY(group_code) REFERENCES Groups(code);
+CREATE TABLE viewers(email text, nikname text, firstname text, lastname text, age int, country text);
+ALTER TABLE viewers
+ADD CONSTRAINT email_pk PRIMARY KEY(email);
 
-CREATE TABLE StudentStatus(group_code text, study_book text, status text,
-						   destiny text, actual_date DATE);
-ALTER TABLE StudentStatus
-ADD CONSTRAINT time_spooky_bk PRIMARY KEY(group_code, study_book, actual_date),
-ADD CONSTRAINT group_spooky_fk FOREIGN KEY(group_code, study_book) REFERENCES Students(group_code, study_book);
+CREATE TABLE products(name_product text, price int, provider_name_provider text);
+ALTER TABLE products
+ADD CONSTRAINT name_products_pk PRIMARY KEY(name_product),
+ADD CONSTRAINT provider_name_provider_fk FOREIGN KEY(provider_name_provider) REFERENCES providers (name_provider);
 
-CREATE TABLE SubjectSheet(subj_name text, group_code text, study_book text,
-						  date_of_mark DATE, mark numeric);
-ALTER TABLE SubjectSheet
-ADD CONSTRAINT time_group_spooky_subj_bk PRIMARY KEY(subj_name, group_code,
-											   study_book, date_of_mark),
-ADD CONSTRAINT subj_name_fk FOREIGN KEY(subj_name) REFERENCES Subjects(name),
-ADD CONSTRAINT group_spooky_fk FOREIGN KEY(group_code, study_book) REFERENCES Students(group_code, study_book);
+CREATE TABLE advs(name_adv text, description text, products_name_product text);
+ALTER TABLE advs
+ADD CONSTRAINT name_adv_pk PRIMARY KEY(name_adv),
+ADD CONSTRAINT products_name_product_fk FOREIGN KEY(products_name_product) REFERENCES products (name_product);
 
-CREATE TABLE Group_Subject(year int, semester int, group_code text, subj_name text);
-ALTER TABLE Group_Subject
-ADD CONSTRAINT group_subj_time_pk PRIMARY KEY(group_code, subj_name, year, semester),
-ADD	CONSTRAINT group_code_fk FOREIGN KEY (group_code) REFERENCES Groups(code),
-ADD	CONSTRAINT subj_name_fk FOREIGN KEY (subj_name) REFERENCES Subjects(name);
+CREATE TABLE ViewersCanProducts(transasction text, product_name_product text, product_price int, viewers_email text);
+ALTER TABLE ViewersCanProducts
+ADD CONSTRAINT transasction_pk PRIMARY KEY(transasction),
+ADD CONSTRAINT products_name_product_fk FOREIGN KEY(products_name_product) REFERENCES products (name_product),
+ADD CONSTRAINT products_price_fk FOREIGN KEY(products_price) REFERENCES products (price),
+ADD CONSTRAINT viewers_email_fk FOREIGN KEY(viewers_email) REFERENCES viewers (email);
--- postgresql/populate.sql
+++ postgresql/populate.sql
@@ -1,34 +1,64 @@
-INSERT INTO Groups VALUES('KM-61');
-INSERT INTO Groups VALUES('KM-62');
-INSERT INTO Groups VALUES('KM-63');
-INSERT INTO Groups VALUES('KM-64');
-
-INSERT INTO Subjects VALUES('Math');
-INSERT INTO Subjects VALUES('English');
 
 
-INSERT INTO Students VALUES('Volodymyr', 'Drapak', 'TBD', 'KM-61');
-INSERT INTO Students VALUES('Random', 'Guy',  'TBD', 'KM-62');
-INSERT INTO Students VALUES('Always', 'Absent', 'otchislen', 'KM-63');
+INSERT INTO advs (name_adv, description, products_name_product)
+VALUES('adv123', 'i love cofe Jacobs minlin', 'Jacobs minlin1');
+INSERT INTO advs (name_adv, description, products_name_product)
+VALUES('fdjadv-327', 'i love cofe Jacobs black', 'Jacobs black');
+INSERT INTO advs (name_adv, description, products_name_product)
+VALUES('43-327', 'i love cofe Neskafe black', 'Neskafe black');
+INSERT INTO advs (name_adv, description, products_name_product)
+VALUES('dfd', 'i  like cofe Neskafe black', 'Neskafe black');
 
-INSERT INTO SubjectSheet VALUES('Math', 'KM-61', 'KM6103', TO_DATE('28.10.19', 'DD.MM.YY'), 100);
-INSERT INTO SubjectSheet VALUES('English', 'KM-61', 'KM6103', TO_DATE('21.10.19', 'DD.MM.YY'), 100);
-INSERT INTO SubjectSheet VALUES('Ukrainian', 'KM-61', 'KM6103', TO_DATE('21.10.19', 'DD.MM.YY'), -200);
+select * from advs
 
-INSERT INTO group_subject VALUES(2015, 1, 'KM-61', 'Math');
-INSERT INTO group_subject VALUES(2015, 1, 'KM-61', 'English');
-INSERT INTO group_subject VALUES(2015, 2, 'KM-61', 'English');
+INSERT INTO products (name_product, price, provider_name_provider)
+VALUES ('Jacobs minlin', 99,'Jacobs');
+INSERT INTO products (name_product, price, provider_name_provider)
+VALUES ('Jacobs black', 89,'Jacobs');
+INSERT INTO products (name_product, price, provider_name_provider)
+VALUES ('Neskafe black', 89,'Nestle');
+INSERT INTO products (name_product, price, provider_name_provider)
+VALUES ('Neskafe mini', 15,'Nestle');
 
-INSERT INTO studentstatus VALUES('KM6103', 'handsome', 'scholarship', TO_DATE('11.11.19', 'DD.MM.YY'));
-INSERT INTO studentstatus VALUES('KM6200', 'good', 'otchislen', TO_DATE('11.11.19', 'DD.MM.YY'));
-INSERT INTO studentstatus VALUES('KM6250', 'handsome', 'dopobachennya', TO_DATE('11.11.19', 'DD.MM.YY'));
-
-INSERT INTO subjects_marks VALUES('Math', 10, TO_DATE('11.11.19', 'DD.MM.YY'));
-INSERT INTO subjects_marks VALUES('English', 50, TO_DATE('11.11.19', 'DD.MM.YY'));
-INSERT INTO subjects_marks VALUES('Economy', 40, TO_DATE('11.11.19', 'DD.MM.YY'));
+select * from products
 
 
-SELECT * FROM Groups;
-SELECT * FROM Subjects;
-SELECT * FROM Students;
-SELECT * FROM SubjectSheet;
+INSERT INTO providers (name_provider, type_product)
+VALUES('Jacobs', 'Coffe');
+INSERT INTO providers (name_provider, type_product)
+VALUES('Nestle', 'Coffe');
+INSERT INTO providers (name_provider, type_product)
+VALUES('ProcteAndGambel', 'Сandy');
+
+
+
+
+INSERT INTO viewers (nikname, email, firstname, lastname, age, country)
+VALUES('I am a superman', 'udsjjh@gmail.com','Oleg','Dobrov', 23, 'Ukraine');
+INSERT INTO viewers (nikname, email, firstname, lastname, age, country)
+VALUES('kata123', 'katusha@gmail.com','Kateryna','Dolib', 25, 'Ukraine');
+INSERT INTO viewers (nikname, email, firstname, lastname, age, country)
+VALUES('BigBoss', 'Dorian@gmail.com','Dorian','Horodskiy', 38, 'Englend');
+INSERT INTO viewers (nikname, email, firstname, lastname, age, country)
+VALUES('LitelGirl', 'AllaMandra@gmail.com','Alla','Mandra', 15, 'Ukraine');
+
+select * from viewers
+
+
+
+INSERT INTO ViewersCanProducts (transasction, product_name_product, product_price, viewers_email)
+VALUES('647249343', 'Jacobs black', '89', 'udsjjh@gmail.com');
+INSERT INTO ViewersCanProducts (transasction, products_name_product, products_price, viewers_email)
+VALUES('kl430920k-43', 'Jacobs minlin', '99', 'udsjjh@gmail.com');
+
+INSERT INTO ViewersCanProducts (transasction, products_name_product, products_price, viewers_email)
+VALUES('fdkl-438-278', 'Jacobs minlin', '99', 'Dorian@gmail.com');
+INSERT INTO ViewersCanProducts (transasction, products_name_product, products_price, viewers_email)
+VALUES('fdkl-438-279', 'Jacobs minlin', '99', 'Dorian@gmail.com');
+
+INSERT INTO ViewersCanProducts (transasction, products_name_product, products_price, viewers_email)
+VALUES('fdkl-438-280', 'Jacobs minlin', '99', 'Dorian@gmail.com');
+
+
+
+select * from ViewersCanProducts
