794759eb600048609915c551679722e779b31e45
Yuliia Kovalchuk <uliakovalchook@gmail.com>
2020-01-08 15:30:04 +0200

    -

--- WTForms.py
+++ WTForms.py
@@ -6,64 +6,20 @@ from wtforms.validators import NumberRange, Required, DataRequired
 
 
 
-class GroupsForm(FlaskForm):
-
-    code = StringField("Code: ", [validators.data_required("Please, enter a code of the group.")])
-
-    submit = SubmitField("Enter")
-
-
-
-class SubjectsForm(FlaskForm):
-
-    name = StringField("Name: ", [validators.data_required("Please, enter a name of the subject.")])
-    submit = SubmitField("Enter")
-
-
-
-class Subjects2Form(FlaskForm):
-
-    predmet = StringField("Predmet: ", [validators.data_required("Please, enter a name of the predmet.")])
-
-    submit = SubmitField("Enter")
-
-
-
-class StudentsForm(FlaskForm):
-
-    first_name = StringField("First name: ", [validators.data_required("Please, enter a first name of the student.")])
-    last_name = StringField("Last name: ", [validators.data_required("Please, enter a last name of the student.")])
-    study_book = StringField("Study book: ", [validators.data_required("Please, enter a study book of the student.")])
-    group_code = StringField("Group code: ", [validators.data_required("Please, enter a group code of the student.")])
-
-    submit = SubmitField("Enter")
-
-
-
 class ProvidersForm(FlaskForm):
 
-    name_provider = StringField("name_provider: ", [validators.data_required("Please, enter a first name of the student.")])
-    type_product = StringField("type_product : ", [validators.data_required("Please, enter a last name of the student.")])
+    name_provider = StringField("name_provider: ", [validators.data_required("Please, enter a first name_provider.")])
+    type_product = StringField("type_product : ", [validators.data_required("Please, enter a last type_product.")])
 
     submit = SubmitField("Enter")
 
-
-class SubjectSheetForm(FlaskForm):
-
-    subj_name = StringField("Subject Name: ", [validators.data_required("Please, enter a name of the subject.")])
-    group_code = StringField("Group code: ", [validators.data_required("Please, enter a group code of the student.")])
-    study_book = StringField("Study book: ", [validators.data_required("Please, enter a study book of the student.")])
-    date_of_mark = DateField("Date of Mark: ", [validators.data_required("Please, enter a date of the mark.")])
-    mark = FloatField("Mark: ", [validators.data_required("Please, enter the mark.")])
-
-    submit = SubmitField("Enter")
 
 
 class ProductsForm(FlaskForm):
 
-    name_product = StringField("name_product: ", [validators.data_required("Please, enter a name of the subject.")])
-    price = StringField("price: ", [validators.data_required("Please, enter a group code of the student.")])
-    provider_name_provider = StringField("provider_name_provider: ", [validators.data_required("Please, enter a study book of the student.")])
+    name_product = StringField("name_product: ", [validators.data_required("Please, enter a name_product.")])
+    price = StringField("price: ", [validators.data_required("Please, enter a price.")])
+    provider_name_provider = StringField("provider_name_provider: ", [validators.data_required("Please, enter a name_provider.")])
 
     submit = SubmitField("Enter")
 
@@ -71,8 +27,8 @@ class ProductsForm(FlaskForm):
 
 class AdvsForm(FlaskForm):
 
-    name_adv = StringField("name_adv: ", [validators.data_required("Please, enter a name of the subject.")])
-    description = StringField("description: ", [validators.data_required("Please, enter a group code of the student.")])
+    name_adv = StringField("name_adv: ", [validators.data_required("Please, enter a name_adv.")])
+    description = StringField("description: ", [validators.data_required("Please, enter a description.")])
     products_name_product = StringField("products_name_product: ", [validators.data_required("Please, enter a name product.")])
 
     submit = SubmitField("Enter")
